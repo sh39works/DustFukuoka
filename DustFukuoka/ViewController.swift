4fb29f388e5afbd2cffb5fbdcb4e20ec009cc8f2
groove <groove@groove-2.local>
2017-06-23 11:53:48 +0900

    Initial Commit

---
+++ DustFukuoka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DustFukuoka
+//
+//  Created by groove on 2017/06/23.
+//  Copyright © 2017年 sh39works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
